7c0d533cd56868b5e37b3ec6c4cf66289eb5d8cd
g00218200 <guorui913@126.com>
2017-09-24 21:57:12 +0800

    提交

--- ICCP_Main/ICCP/src/KernelService/kernel-service-pkg/kernel-eureka-service/src/main/java/com/nebula/users/EurekaServiceApplication.java
+++ ICCP_Main/ICCP/src/KernelService/kernel-service-pkg/kernel-eureka-service/src/main/java/com/nebula/users/EurekaServiceApplication.java
@@ -2,10 +2,12 @@ package com.nebula.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class EurekaServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceApplication.class, args);
